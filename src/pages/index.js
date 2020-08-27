3a45a5e1adfaa3ff1c0eab45ef291cc2753bc07c
Nick Bansal <extreme_wired@msn.com>
2020-08-27 10:59:30 +0100

    updated the site and deleted unneeded files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import '../styles/styles.scss';
 
 const IndexPage = () => (
-	<Layout>
-		<SEO title="Home" />
-		<div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-			<Image />
-		</div>
-		<Link to="/using-typescript/">Go to &quot;Using TypeScript&quot;</Link>
-	</Layout>
+	<p>Home</p>
 );
 
 export default IndexPage;
