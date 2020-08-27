cdf97bd9fdafce15ada350083cad65ae8f58d5ad
Nick Bansal <extreme_wired@msn.com>
2020-08-27 11:22:12 +0100

    added styling to the header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../styles/styles.scss';
 
+import Header from '../components/header';
+
 const IndexPage = () => (
-	<p>Home</p>
+	<Header />
 );
 
 export default IndexPage;
